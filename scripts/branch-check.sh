d57562d4319450fd8e3c1846715f2b5a4f9a8c7c
Alessandro Russo <alerusso.mail@libero.it>
2022-05-31 17:59:21 +0200

    Pre-push checks

--- scripts/branch-check.sh
+++ scripts/branch-check.sh
@@ -2,14 +2,13 @@
 # Check if you can push on some branches
 set -e
 branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+echo "You are pushing in ---> $branch"
 
 # Edit here if you don't want to be pushed on a certain branches
 if [ "$branch" != "dontpushhere" ] && [ "$branch" != "anddontpushhere" ]; then
-  echo "You are pushing in: $branch"
   echo "You can push!"
 
 else
-  echo "You are pushing in: $branch"
   echo "You cannot push! Check ./scripts/can-push-check.sh"
   exit 1
 fi
--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 # This will update your project version number
 # based on your branch name
-# If it's a hotfix/branch-name or bugfix/branch-name or fix/branch-name it will make a patch
+# If it's the branch name contain fix it will make a patch
 # If it's a feature/branch-name it will make a minor
 
 #!/bin/bash
-branch=$(git branch --show-current | sed -n -e 's/^\* \(.*\)/\1/p')
-echo "$branch"
-if [[ "feature" == *"$branch"* ]]; then
-  echo "feature"
-elif
-  [[ "fix" == *"$branch"* ]] || [[ "bugfix" == *"$branch"* ]] || [[ "hotfix" == *"$branch"* ]]; then
-    echo "fix"
+stringContain() { [ -z "$1" ] || { [ -z "${2##*$1*}" ] && [ -n "$2" ];};}
+
+branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+
+if stringContain "feature" $branch; then
+  npm version minor
+elif stringContain "fix" $branch; then
+  npm version patch
 else
   echo "origin"
 fi
