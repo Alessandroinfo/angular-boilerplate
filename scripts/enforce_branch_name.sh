268aac0a7e8026900493fe18bfd1110a4d1874dc
Alessandro Russo <alerusso.mail@libero.it>
2022-06-10 14:59:14 +0200

    Fix package.json

--- scripts/enforce_branch_name.sh
+++ scripts/enforce_branch_name.sh
@@ -7,12 +7,12 @@ LOCAL_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 # This is to enforce the commit only to the branches that start with 'feature|release|bugfix|hotfix|fix'
 VALID_BRANCH_REGEX="^(feature|release|bugfix|hotfix|fix)\/[a-z0-9._-]+$"
 
-MESSAGE="[enforce-branch-name] There is something wrong with your branch name. Branch names must adhere to this contract: $valid_branch_regex. Your commit will be rejected. You should rename your branch to a valid name and try again."
 echo "[enforce-branch-name] You are pushing in --> $LOCAL_BRANCH"
 
 if [[ ! $LOCAL_BRANCH =~ $VALID_BRANCH_REGEX ]]
 then
-    echo "$MESSAGE"
+    echo "[enforce-branch-name] There is something wrong with your branch name. Branch names must adhere to this regex: $VALID_BRANCH_REGEX."
+    echo "[enforce-branch-name] Your commit will be rejected. You should rename your branch to a valid name and try again."
     exit 1
 fi
 
