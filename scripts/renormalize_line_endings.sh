0ea76ee536a6c13bb5db59f1d7303566373c180a
Alessandro Russo <alerusso.mail@libero.it>
2022-06-10 16:12:31 +0200

    Fix package.json

--- scripts/renormalize_line_endings.sh
+++ scripts/renormalize_line_endings.sh
@@ -1,3 +1,5 @@
+# Refreshing a repository after changing line endings
+# https://docs.github.com/en/get-started/getting-started-with-git/configuring-git-to-handle-line-endings
 git add . -u &&
 git commit -m "Saving files before refreshing line endings" &&
 git add --renormalize . &&
