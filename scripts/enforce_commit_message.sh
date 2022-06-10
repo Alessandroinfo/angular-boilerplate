20368b245105515527ad13fa7163c2c1e4a4d44b
Alessandro Russo <alerusso.mail@libero.it>
2022-06-10 12:39:15 +0200

    fix package.json

--- scripts/enforce_commit_message.sh
+++ scripts/enforce_commit_message.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # Allow commit messages that start with a uppercase letter
-BRANCH_NAME=$(git symbolic-ref --short HEAD)
 INPUT_FILE=$1
 echo $1
 COMMIT_MSG=`head -n1 $INPUT_FILE`
+echo $COMMIT_MSG
 REGEX_TO_MATCH="^[A-Z]"
 if ! [[ "$COMMIT_MSG" =~ $REGEX_TO_MATCH ]]; then
-  echo $COMMIT_MSG
   echo "[enforce-commit-message] Your commit message is illegal. Please rename your branch with using following regex: $REGEX_TO_MATCH"
   exit 1
 fi
