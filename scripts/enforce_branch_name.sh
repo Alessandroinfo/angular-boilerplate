02b5931a51dcc65ef26dbf9986708570152392ad
Alessandro Russo <alerusso.mail@libero.it>
2022-06-10 13:09:28 +0200

    F

--- scripts/enforce_branch_name.sh
+++ scripts/enforce_branch_name.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 LC_ALL=C
 
-local_branch="$(git rev-parse --abbrev-ref HEAD)"
+LOCAL_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 
 # Put here regex for the branches to enforce
 # This is to enforce the commit only to the branche that start with 'feature|improvement|library|prerelease|release|bugfix|hotfix|fix'
-valid_branch_regex="^(feature|release|bugfix|hotfix|fix)\/[a-z0-9._-]+$"
+VALID_BRANCH_REGEX="^(feature|release|bugfix|hotfix|fix)\/[a-z0-9._-]+$"
 
-message="[enforce-branch-name] There is something wrong with your branch name. Branch names must adhere to this contract: $valid_branch_regex. Your commit will be rejected. You should rename your branch to a valid name and try again."
+MESSAGE="[enforce-branch-name] There is something wrong with your branch name. Branch names must adhere to this contract: $valid_branch_regex. Your commit will be rejected. You should rename your branch to a valid name and try again."
 
-if [[ ! $local_branch =~ $valid_branch_regex ]]
+if [[ ! $LOCAL_BRANCH =~ $VALID_BRANCH_REGEX ]]
 then
-    echo "[enforce-branch-name] You are pushing in --> $branch"
-    echo "$message"
+    echo "[enforce-branch-name] You are pushing in --> $LOCAL_BRANCH"
+    echo "$MESSAGE"
     exit 1
 fi
 
--- scripts/enforce_commit_message.sh
+++ scripts/enforce_commit_message.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Allow commit messages that start with a uppercase letter
+# Change the REGEX_TO_MATCH how you want
 INPUT_FILE=$1
 COMMIT_MSG=`head -n1 $INPUT_FILE`
 REGEX_TO_MATCH="^[A-Z]"
