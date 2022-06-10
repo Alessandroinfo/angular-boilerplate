350eac7ad3eb970e7a470f8c8d5065c3ad07b55e
Alessandro Russo <alerusso.mail@libero.it>
2022-06-10 13:07:10 +0200

    F

--- scripts/enforce_commit_message.sh
+++ scripts/enforce_commit_message.sh
@@ -2,11 +2,9 @@
 
 # Allow commit messages that start with a uppercase letter
 INPUT_FILE=$1
-echo $1
 COMMIT_MSG=`head -n1 $INPUT_FILE`
-echo $COMMIT_MSG
 REGEX_TO_MATCH="^[A-Z]"
-if ! [[ "$COMMIT_MSG" =~ $REGEX_TO_MATCH ]]; then
-  echo "[enforce-commit-message] Your commit message is illegal. Please rename your branch with using following regex: $REGEX_TO_MATCH"
+if [[ ! $COMMIT_MSG =~ $REGEX_TO_MATCH ]]; then
+  echo "[enforce-commit-message] Your commit message ($COMMIT_MSG) is illegal. Please rename your branch with using following regex: $REGEX_TO_MATCH"
   exit 1
 fi
