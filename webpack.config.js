28f9fa81cedd59c3a0175852b02e358634efca93
Alessandro Russo <alerusso.mail@libero.it>
2022-06-16 13:00:20 +0200

    Update angular version

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,16 @@
-const {patchPostCSS} = require('@ngneat/tailwind');
+const { patchPostCSS } = require("@ngneat/tailwind");
+const tailwindConfig = require("./tailwind.config.js");
 
 module.exports = (config) => {
   const isProd = config.mode === 'production';
   const tailwindConfig = require('./tailwind.config.js')(isProd);
   patchPostCSS(config, tailwindConfig, true);
+  patchAngularPostCSS({
+    webpackConfig: config,
+    addPlugins: [
+      require("postcss-preset-env"),
+      require("postcss-css-variables")
+    ],
+  });
   return config;
 };
