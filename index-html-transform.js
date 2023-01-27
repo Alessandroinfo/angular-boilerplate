07548cc4ff2020f59744450a5d014a89cefd8d73
Alessandro Russo <alessandro.russo@datlas.it>
2023-01-27 19:30:51 +0100

    Added on the index.html file correct comment indentation. To prevent prettier to add newline. Started on the webpack.config.js the management of the loader css script without beeing a render-blocking resource

--- index-html-transform.js
+++ index-html-transform.js
@@ -1,37 +1,6 @@
 module.exports = (targetOptions, indexHtml) => {
   let newIndexHtml = indexHtml;
 
-  /*
-   * Preloaded CSS resource defer-non-critical-css
-   * https://web.dev/defer-non-critical-css/
-   */
-  console.log(indexHtml);
-  debugger;
-  // Regex for getting the whole link tag if there is a stylesheet word
-  const linkRegExp = /<link.*?stylesheet?.*?\s*?>/;
-
-  // If match with the index
-  const match = newIndexHtml.match(linkRegExp);
-
-  if (match.length) {
-    // Getting <link> tag
-    let linkHTMLTag = match[0];
-
-    // Remove media type because we load css with preload
-    // linkHTMLTag = linkHTMLTag.replace(
-    //   'rel="stylesheet"',
-    //   'rel="preload" as="style"'
-    // );
-    // Edits the tag to be: rel="preload", as="style", onload="this.media='all';this.onload=null;this.rel='stylesheet'"
-    linkHTMLTag = linkHTMLTag.replace('rel="stylesheet"', 'rel="preload"');
-    linkHTMLTag = linkHTMLTag.replace(
-      new RegExp('onload=".*"', 'gm'),
-      "onload=\"this.media='all';this.rel='stylesheet';this.onload=null\""
-    );
-
-    newIndexHtml = indexHtml.replace(new RegExp(linkRegExp), linkHTMLTag);
-  }
-
   // Cordova index management
   // TODO: Check if this it's correct when enabled
   if (
