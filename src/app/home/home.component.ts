b2b3f936a804f40651bb7c113baf3fdfb99ec51c
Alessandro Russo <alessandro.russo@datlas.it>
2023-02-04 17:38:12 +0100

    Material fix

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,6 +9,35 @@ declare const APP_VERSION: string;
 @Component({
   selector: 'app-home',
   template: `
+    <div>
+      <h1>Material buttons</h1>
+
+      <div class="example-label">Basic</div>
+      <div class="example-button-row">
+        <button mat-button>Basic</button>
+        <button mat-button color="primary">Primary</button>
+        <button mat-button color="accent">Accent</button>
+        <button mat-button color="warn">Warn</button>
+        <button mat-button disabled>Disabled</button>
+        <a mat-button href="https://www.google.com/" target="_blank">Link</a>
+      </div>
+
+      <mat-divider></mat-divider>
+
+      <div class="example-label">Raised</div>
+      <div class="example-button-row">
+        <button mat-raised-button>Basic</button>
+        <button mat-raised-button color="primary">Primary</button>
+        <button mat-raised-button color="accent">Accent</button>
+        <button mat-raised-button color="warn">Warn</button>
+        <button mat-raised-button disabled>Disabled</button>
+        <a mat-raised-button href="https://www.google.com/" target="_blank">
+          Link
+        </a>
+      </div>
+    </div>
+    <br />
+
     <button mat-raised-button routerLink="/home" color="primary">HOME</button>
 
     <button mat-raised-button routerLink="/about" color="primary">ABOUT</button>
